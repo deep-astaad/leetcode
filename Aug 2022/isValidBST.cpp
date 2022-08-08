99133d16ab83ff2892d889eaf98300d1c918d739
deep_aman <deepamansinghads@gmail.com>
2022-08-08 12:04:32 +0530

    08-08-2022

---
+++ Aug 2022/isValidBST.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+        bool checkValidBST(TreeNode *root, int &minV, int &maxV)
+        {
+            int temp;
+            minV = maxV = root->val;
+            if(root->left)
+            {
+                if( !checkValidBST(root->left, minV, temp) || temp >= root->val)  
+                    return false;
+            }
+    
+            if(root->right)
+            {
+                if( !checkValidBST(root->right, temp, maxV) || temp <= root->val)  
+                    return false;
+            }
+            
+            return true;
+        }
+    
+        bool isValidBST(TreeNode *root) {
+            int minV, maxV;
+            if(root)
+            {
+                return checkValidBST(root, minV, maxV);
+            }
+            else
+            {
+                return true;
+            }
+        }
+    };
