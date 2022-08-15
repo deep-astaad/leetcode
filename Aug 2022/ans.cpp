9014a71992e7f127e19ce9a06d1bb97e0b46ec65
deep_aman <deepamansinghads@gmail.com>
2022-08-15 23:21:53 +0530

    15-08-2022

---
+++ Aug 2022/ans.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main()
+{
+    
+    ll n;
+    cin>>n;
+    n=10;
+    int fm=0,fl=0,count=0;
+    while(n>0){
+    	if(n||0){
+    		count++;
+    	}
+    }
+    cout<<count;
+    return 0;
+}
+
