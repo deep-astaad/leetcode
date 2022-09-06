ac34630a4badcd47e96315f9418ac38e8b101c75
Aman Deep Singh <84132743+deep-astaad@users.noreply.github.com>
2022-09-06 12:04:51 +0530

    Rotate Array Recursive added

--- Aug 2022/rotateArray(add_mem).cpp
+++ Aug 2022/rotateArray(add_mem).cpp
@@ -14,3 +14,14 @@ public:
         }
     }
 };
+
+// void rotate(vector<int>& nums, int k) {
+    //     k = k % nums.size();
+    //     if(k==0) return;
+    //     int t = nums[nums.size()-1]; //t=7
+    //     for(int i=nums.size()-1;i>0;i--){
+    //         nums[i]=nums[i-1];
+    //     }
+    //     nums[0] = t;
+    //     rotate(nums,--k);
+// }
